a82cbb6a6f1c8ee298619a6d19f17720332f6e20
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-04-28 22:22:02 -0300

    feat: Adding Contact, useMutation, AutoRefresh, Cash

--- app-agenda/src/context/ContatosContext.jsx
+++ app-agenda/src/context/ContatosContext.jsx
@@ -1,16 +1,31 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { createContext, useContext } from "react";
-import { GET_CONTATOS } from "../graphql";
+import { ADD_CONTATO, GET_CONTATOS } from "../graphql";
 
 const MyContext = createContext();
 
 export default function ContatosContextProvider({ children }) {
   const { data, loading, error } = useQuery(GET_CONTATOS);
 
+  const [criarContato] = useMutation(ADD_CONTATO, {
+    update(cache, { data }) {
+      const newContatoResponse = data?.criarContato;
+      const existingContatos = cache.readQuery({ query: GET_CONTATOS });
+
+      cache.writeQuery({
+        query: GET_CONTATOS,
+        data: {
+          contatos: [...existingContatos.contatos, newContatoResponse],
+        },
+      });
+    },
+  });
+
   const contatos = {
     itens: data?.contatos ?? [],
     loading,
     error,
+    criarContato,
   };
 
   return (
